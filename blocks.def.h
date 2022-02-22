9d7b32ebe604b8adb996f61c87d66b1019ea36e9
russell-ali <resul.eliyev2000@mail.ru>
2022-02-23 02:45:28 +0400

    white space

--- blocks.def.h
+++ blocks.def.h
@@ -1,16 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "date +%R", 60, 0},
+	{"  ", "date '+%b %e %a %R  '", 60, 0},
 	{"", "blk-battery", 5, 0},
 	{"", "blk-volume", 0, 1},
 	{"", "blk-backlight", 0, 2},
 	{"", "blk-layout", 0, 3},
-	{"", "blk-cputemp", 5, 0},
-	{"", "blk-cpupercent", 5, 0},
+	{"", "blk-cpu", 5, 0},
 	{"", "blk-memory", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
